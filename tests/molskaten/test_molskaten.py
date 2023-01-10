07b58fb0e563e2875c60066773450c6715d762fd
granis <granis@gmail.com>
2023-01-10 18:32:25 +0100

    tests: cover Lunch

--- tests/molskaten/test_molskaten.py
+++ tests/molskaten/test_molskaten.py
@@ -1,9 +1,16 @@
 """test_molskaten.py."""
+from datetime import datetime
 
-from molskaten import Molskaten
+from molskaten import Lunch, Molskaten
 
 
 def test_molskaten_list_result() -> None:
     mol = Molskaten("dalviksskolan")
     data = mol.getData()
     assert isinstance(data, list)  # nosec assert_used
+
+
+def test_lunch() -> None:
+    lunch = Lunch("Meatrolls", datetime(2022, 1, 1, 0, 0, 0))
+    assert str(lunch) == "Lunch: 2022-01-01 @ Meatrolls"
+    assert lunch.food == "Meatrolls"
